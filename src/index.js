71f2ccfbcf8b23eaa475e583c159d28937f976c0
Adi Levi <adile0194@gmail.com>
2024-03-01 16:46:18 +0200

    added empty file form component

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import reportWebVitals from './reportWebVitals';
-import Header from './components/Header/Header';
 import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Header />
+    <App />
   </React.StrictMode>
 );
 
