349d32b711fb699d2cdae843b0e96faa1ad10d00
Adi Levi <adile0194@gmail.com>
2024-03-03 17:23:01 +0200

    update router

--- server/routers/firstaid.router.js
+++ server/routers/firstaid.router.js
@@ -3,10 +3,10 @@ const { firstAidController } = require('../controllers/firstaid.controller');
 
 const firstAidRouter = new Router();
 
-firstAidRouter.get('/', firstAidController.getAllFirstAid);
-firstAidRouter.get('/:firstAidId', firstAidController.getFirstAidById);
-firstAidRouter.post('/', firstAidController.createFirstAid);
-firstAidRouter.put('/:firstAidId', firstAidController.updateFirstAid);
-firstAidRouter.delete('/:firstAidId', firstAidController.deleteFirstAid);
+firstAidRouter.get('/', firstAidController.getAllFirstAidInstructions);
+firstAidRouter.get('/:firstAidId', firstAidController.getFirstAidInstruction);
+firstAidRouter.post('/', firstAidController.createFirstAidInstruction);
+firstAidRouter.put('/:firstAidId', firstAidController.updateFirstAidInstructions);
+firstAidRouter.delete('/:firstAidId', firstAidController.deleteFirstAidInstructions);
 
 module.exports = { firstAidRouter };
