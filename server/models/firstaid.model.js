b367280237eb7350863c860a2f604b5f90e5587d
MoranSin <nurixhbh@gmail.com>
2024-03-03 21:27:48 +0200

    model cleaned

--- server/models/firstaid.model.js
+++ server/models/firstaid.model.js
@@ -1,9 +1,5 @@
 const { Schema, model } = require('mongoose');
 
-// const instructionSchema = new Schema({
-//     instruction: { type: Array, required: true }
-// }, { collection: 'instructions' });
-
 const firstAidSchema = new Schema({
 injuryType: { type: String, required: true },
 description: { type: String, required: true },
