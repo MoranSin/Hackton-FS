d5059540d0899f88b5bbb2703c26cb69ad455ffd
MoranSin <nurixhbh@gmail.com>
2024-03-03 18:29:57 +0200

    model fixes

--- server/models/firstaid.model.js
+++ server/models/firstaid.model.js
@@ -7,7 +7,7 @@ const { Schema, model } = require('mongoose');
 const firstAidSchema = new Schema({
 injuryType: { type: String, required: true },
 description: { type: String, required: true },
-    instructions:[String], required:true,
+    instructions:{ type: [String], required: true },
 }, { collection: 'FirstAid' });
 
 module.exports = model('firstAid', firstAidSchema);
